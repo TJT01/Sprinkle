61384efd1e668d70157d86fee71212e333482c96
Trevor Tudor <trevortudor2001@gmail.com>
2021-07-29 14:32:55 -0400

    Do a whole bunch of things

---
+++ src/main/java/mod/tjt01/sprinkle/data/QuarkFlagCondition.java
@@ -0,0 +1,54 @@
+package mod.tjt01.sprinkle.data;
+
+import com.google.gson.JsonObject;
+import net.minecraft.util.JSONUtils;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.crafting.CraftingHelper;
+import net.minecraftforge.common.crafting.conditions.ICondition;
+import net.minecraftforge.common.crafting.conditions.IConditionSerializer;
+import net.minecraftforge.fml.ModList;
+
+public class QuarkFlagCondition implements ICondition {
+    private static final ResourceLocation NAME = new ResourceLocation("sprinkle", "quark_flag");
+    private final String flag;
+
+    public QuarkFlagCondition(String flag) {
+        this.flag = flag;
+    }
+
+    @Override
+    public ResourceLocation getID() {
+        return NAME;
+    }
+
+    @Override
+    public boolean test() {
+        if (ModList.get().isLoaded("quark")) {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("type", "quark:flag");
+            jsonObject.addProperty("flag", this.flag);
+            return CraftingHelper.getCondition(jsonObject).test();
+        }
+        return false;
+    }
+
+    public static class Serializer implements IConditionSerializer<QuarkFlagCondition> {
+        private static final ResourceLocation NAME = new ResourceLocation("sprinkle", "quark_flag");
+        public static final Serializer INSTANCE = new Serializer();
+
+        @Override
+        public void write(JsonObject json, QuarkFlagCondition value) {
+            json.addProperty("flag", value.flag);
+        }
+
+        @Override
+        public QuarkFlagCondition read(JsonObject json) {
+            return new QuarkFlagCondition(JSONUtils.getAsString(json, "flag"));
+        }
+
+        @Override
+        public ResourceLocation getID() {
+            return NAME;
+        }
+    }
+}
