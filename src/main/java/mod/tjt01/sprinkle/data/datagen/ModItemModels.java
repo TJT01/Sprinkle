561ae93f0b3aa8f68df56f6ad6fb44a79751179b
Trevor Tudor <trevortudor2001@gmail.com>
2022-01-27 16:42:26 -0500

    yay it compiles

--- src/main/java/mod/tjt01/sprinkle/data/datagen/ModItemModels.java
+++ src/main/java/mod/tjt01/sprinkle/data/datagen/ModItemModels.java
@@ -11,10 +11,6 @@ public class ModItemModels extends ItemModelProvider {
 
     @Override
     protected void registerModels() {
-        this.singleTexture("bundle", mcLoc("item/generated"), "layer0", modLoc("item/bundle"))
-                .override()
-                .model(this.singleTexture("bundle_filled", mcLoc("item/generated"), "layer0", modLoc("item/bundle_filled")))
-                .predicate(modLoc("filled"), 1e-7f)
-                .end();
+
     }
 }
