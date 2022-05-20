a3012ab3469e0215dc606adfdf6433b77b93ea1c
Trevor Tudor <trevortudor2001@gmail.com>
2022-05-20 14:16:57 -0400

    nightshale recipes

--- src/main/java/mod/tjt01/sprinkle/data/datagen/Recipes.java
+++ src/main/java/mod/tjt01/sprinkle/data/datagen/Recipes.java
@@ -2,11 +2,13 @@ package mod.tjt01.sprinkle.data.datagen;
 
 import java.util.function.Consumer;
 
+import mod.tjt01.lapislib.data.OptionalRecipeBuilder;
 import mod.tjt01.sprinkle.data.FlagCondition;
 import mod.tjt01.sprinkle.data.QuarkFlagCondition;
 import mod.tjt01.sprinkle.init.ModBlocks;
 import mod.tjt01.sprinkle.init.ModItems;
 import net.minecraft.data.*;
+import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.ItemLike;
@@ -50,17 +52,36 @@ class Recipes extends RecipeProvider {
         return this.oneByThree(result, Ingredient.of(ingredient), count);
     }
 
+    protected ShapedRecipeBuilder stairs(ItemLike result, Ingredient ingredient) {
+        return ShapedRecipeBuilder.shaped(result, 4)
+                .pattern("#  ")
+                .pattern("## ")
+                .pattern("###")
+                .define('#', ingredient);
+    }
+
+    protected ShapedRecipeBuilder stairs(ItemLike result, ItemLike ingredient) {
+        return this.stairs(result, Ingredient.of(ingredient));
+    }
+
+    protected void simpleStonecutting(ItemLike ingredient, ItemLike output, int count, Consumer<FinishedRecipe> consumer) {
+        String ingredientName = ingredient.asItem().getRegistryName().getPath();
+        SingleItemRecipeBuilder.stonecutting(Ingredient.of(ingredient), output, count)
+                .unlockedBy("has_" + ingredientName, has(ingredient))
+                .save(consumer, new ResourceLocation("sprinkle", output.asItem().getRegistryName().getPath() + "_from_" + ingredientName + "_stonecutting"));
+    }
+
+    protected void simpleStonecutting(ItemLike ingredient, ItemLike output, Consumer<FinishedRecipe> consumer) {
+        this.simpleStonecutting(ingredient, output, 1, consumer);
+    }
+
     @Override
     protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
         this.twoByTwo(ModBlocks.PURPUR_BRICKS.get(), Items.PURPUR_BLOCK, 4)
                 .unlockedBy("has_purpur_block", has(Items.PURPUR_BLOCK))
                 .save(consumer);
 
-        ShapedRecipeBuilder.shaped(ModBlocks.PURPUR_BRICK_STAIRS.get(), 4)
-                .pattern("#  ")
-                .pattern("## ")
-                .pattern("###")
-                .define('#', ModBlocks.PURPUR_BRICKS.get())
+        this.stairs(ModBlocks.PURPUR_BRICK_STAIRS.get(), ModBlocks.PURPUR_BRICKS.get())
                 .unlockedBy("has_purpur_bricks", has(ModBlocks.PURPUR_BRICKS.get()))
                 .save(consumer);
 
@@ -73,6 +94,49 @@ class Recipes extends RecipeProvider {
                 .pattern("###")
                 .define('#', ModBlocks.PURPUR_BRICKS.get())
                 .unlockedBy("has_purpur_bricks", has(ModBlocks.PURPUR_BRICKS.get()))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(ModBlocks.NIGHTSHALE.get(), 4)
+                .pattern("###")
+                .pattern("#I#")
+                .pattern("###")
+                .define('#', Items.FLINT)
+                .define('I', Tags.Items.GEMS_LAPIS)
+                .unlockedBy("has_flint", has(Items.FLINT))
+                .save(consumer);
+
+        this.stairs(ModBlocks.NIGHTSHALE_STAIRS.get(), ModBlocks.NIGHTSHALE.get())
+                .unlockedBy("has_nightshale", has(ModBlocks.NIGHTSHALE.get()))
+                .save(consumer);
+
+        this.oneByThree(ModBlocks.NIGHTSHALE_SLAB.get(), ModBlocks.NIGHTSHALE.get(), 6)
+                .unlockedBy("has_nightshale", has(ModBlocks.NIGHTSHALE.get()))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(ModBlocks.NIGHTSHALE_WALL.get(), 6)
+                .pattern("###")
+                .pattern("###")
+                .define('#', ModBlocks.NIGHTSHALE.get())
+                .unlockedBy("has_nightshale", has(ModBlocks.NIGHTSHALE.get()))
+                .save(consumer);
+
+        this.twoByTwo(ModBlocks.NIGHTSHALE_BRICKS.get(), ModBlocks.NIGHTSHALE_BRICKS.get(), 4)
+                .unlockedBy("has_nightshale", has(ModBlocks.NIGHTSHALE_BRICKS.get()))
+                .save(consumer);
+
+        this.stairs(ModBlocks.NIGHTSHALE_BRICK_STAIRS.get(), ModBlocks.NIGHTSHALE_BRICKS.get())
+                .unlockedBy("has_nightshale", has(ModBlocks.NIGHTSHALE_BRICKS.get()))
+                .save(consumer);
+
+        this.oneByThree(ModBlocks.NIGHTSHALE_BRICK_SLAB.get(), ModBlocks.NIGHTSHALE_BRICKS.get(), 6)
+                .unlockedBy("has_nightshale", has(ModBlocks.NIGHTSHALE_BRICKS.get()))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(ModBlocks.NIGHTSHALE_BRICK_WALL.get(), 6)
+                .pattern("###")
+                .pattern("###")
+                .define('#', ModBlocks.NIGHTSHALE_BRICKS.get())
+                .unlockedBy("has_nightshale", has(ModBlocks.NIGHTSHALE_BRICKS.get()))
                 .save(consumer);
 
         ShapedRecipeBuilder.shaped(ModBlocks.DETECTOR.get())
@@ -114,34 +178,34 @@ class Recipes extends RecipeProvider {
                 .unlockedBy("has_gold_ingot", has(Tags.Items.INGOTS_GOLD))
                 .save(consumer);
 
-        SingleItemRecipeBuilder.stonecutting(Ingredient.of(Items.PURPUR_BLOCK), ModBlocks.PURPUR_BRICKS.get())
-                .unlockedBy("has_purpur_block", has(Items.PURPUR_BLOCK))
-                .save(consumer, new ResourceLocation("sprinkle", "purpur_bricks_from_purpur_block_stonecutting"));
+        this.simpleStonecutting(Items.PURPUR_BLOCK, ModBlocks.PURPUR_BRICKS.get(), consumer);
+        this.simpleStonecutting(Items.PURPUR_BLOCK, ModBlocks.PURPUR_BRICK_SLAB.get(), 2, consumer);
+        this.simpleStonecutting(Items.PURPUR_BLOCK, ModBlocks.PURPUR_BRICK_STAIRS.get(), consumer);
+        this.simpleStonecutting(Items.PURPUR_BLOCK, ModBlocks.PURPUR_BRICK_WALL.get(), consumer);
+        this.simpleStonecutting(ModBlocks.PURPUR_BRICKS.get(), ModBlocks.PURPUR_BRICK_SLAB.get(), 2, consumer);
+        this.simpleStonecutting(ModBlocks.PURPUR_BRICKS.get(), ModBlocks.PURPUR_BRICK_STAIRS.get(), consumer);
+        this.simpleStonecutting(ModBlocks.PURPUR_BRICKS.get(), ModBlocks.PURPUR_BRICK_WALL.get(), consumer);
 
-        SingleItemRecipeBuilder.stonecutting(Ingredient.of(Items.PURPUR_BLOCK), ModBlocks.PURPUR_BRICK_SLAB.get(), 2)
-                .unlockedBy("has_purpur_block", has(Items.PURPUR_BLOCK))
-                .save(consumer, new ResourceLocation("sprinkle", "purpur_brick_slab_from_purpur_block_stonecutting"));
+        this.simpleStonecutting(ModBlocks.NIGHTSHALE.get(), ModBlocks.NIGHTSHALE_BRICKS.get(), consumer);
+        this.simpleStonecutting(ModBlocks.NIGHTSHALE.get(), ModBlocks.NIGHTSHALE_BRICK_SLAB.get(), 2, consumer);
+        this.simpleStonecutting(ModBlocks.NIGHTSHALE.get(), ModBlocks.NIGHTSHALE_BRICK_STAIRS.get(), consumer);
+        this.simpleStonecutting(ModBlocks.NIGHTSHALE.get(), ModBlocks.NIGHTSHALE_BRICK_WALL.get(), consumer);
+        this.simpleStonecutting(ModBlocks.NIGHTSHALE_BRICKS.get(), ModBlocks.NIGHTSHALE_BRICK_SLAB.get(), 2, consumer);
+        this.simpleStonecutting(ModBlocks.NIGHTSHALE_BRICKS.get(), ModBlocks.NIGHTSHALE_BRICK_STAIRS.get(), consumer);
+        this.simpleStonecutting(ModBlocks.NIGHTSHALE_BRICKS.get(), ModBlocks.NIGHTSHALE_BRICK_WALL.get(), consumer);
 
-        SingleItemRecipeBuilder.stonecutting(Ingredient.of(Items.PURPUR_BLOCK), ModBlocks.PURPUR_BRICK_STAIRS.get())
-                .unlockedBy("has_purpur_block", has(Items.PURPUR_BLOCK))
-                .save(consumer, new ResourceLocation("sprinkle", "purpur_brick_stairs_from_purpur_block_stonecutting"));
-
-        SingleItemRecipeBuilder.stonecutting(Ingredient.of(Items.PURPUR_BLOCK), ModBlocks.PURPUR_BRICK_WALL.get())
-                .unlockedBy("has_purpur_block", has(Items.PURPUR_BLOCK))
-                .save(consumer, new ResourceLocation("sprinkle", "purpur_brick_wall_from_purpur_block_stonecutting"));
-
-        SingleItemRecipeBuilder.stonecutting(Ingredient.of(ModBlocks.PURPUR_BRICKS.get()), ModBlocks.PURPUR_BRICK_SLAB.get(), 2)
-                .unlockedBy("has_purpur_block", has(ModBlocks.PURPUR_BRICKS.get()))
-                .save(consumer, new ResourceLocation("sprinkle", "purpur_brick_slab_from_purpur_bricks_stonecutting"));
-
-        SingleItemRecipeBuilder.stonecutting(Ingredient.of(ModBlocks.PURPUR_BRICKS.get()), ModBlocks.PURPUR_BRICK_STAIRS.get())
-                .unlockedBy("has_purpur_block", has(Items.PURPUR_BLOCK))
-                .save(consumer, new ResourceLocation("sprinkle", "purpur_brick_stairs_from_purpur_bricks_stonecutting"));
-
-        SingleItemRecipeBuilder.stonecutting(Ingredient.of(ModBlocks.PURPUR_BRICKS.get()), ModBlocks.PURPUR_BRICK_WALL.get())
-                .unlockedBy("has_purpur_block", has(Items.PURPUR_BLOCK))
-                .save(consumer, new ResourceLocation("sprinkle", "purpur_brick_wall_from_purpur_bricks_stonecutting"));
-
+        //TODO fix in LapisLib
+//        OptionalRecipeBuilder.optional(finishedRecipeConsumer -> {
+//            ShapedRecipeBuilder.shaped(ModBlocks.VERTICAL_PURPUR_BRICK_SLAB.get(), 3)
+//                    .pattern("#")
+//                    .pattern("#")
+//                    .pattern("#")
+//                    .define('#', ModBlocks.PURPUR_BRICK_SLAB.get())
+//                    .unlockedBy("has_purpur_brick_slab", has(ModBlocks.PURPUR_BRICK_SLAB.get()))
+//                    .save(finishedRecipeConsumer);
+//        })
+//                .addCondition(new QuarkFlagCondition("vertical_slabs"))
+//                .save(consumer);
         {
             ShapedRecipeBuilder vertSlab = ShapedRecipeBuilder.shaped(ModBlocks.VERTICAL_PURPUR_BRICK_SLAB.get(), 3)
                     .pattern("#")
